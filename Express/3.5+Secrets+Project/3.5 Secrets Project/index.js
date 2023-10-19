fe451aae7268aeb03c364988eca2b311a6dbf08c
Paramjeet <paramjeetnpradhan@gmail.com>
2023-10-19 06:21:06 +0530

    changed authentication as multi pass going through

--- Express/3.5+Secrets+Project/3.5 Secrets Project/index.js
+++ Express/3.5+Secrets+Project/3.5 Secrets Project/index.js
@@ -1,3 +1,42 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var pass=""
+
+
+//sending check password -html page to user 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+
+app.use(bodyParser.urlencoded({ extended:true}));
+
+function passcheck(req,res,next){
+    console.log(req.body);
+    pass=req.body["password"];
+    next();
+  }
+  app.use(passcheck);
+
+ //sending requested thing to client
+  app.post("/check",(req,res)=>{
+    if(pass==="pass"){
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else{
+    res.sendFile(__dirname + "/public/index.html");
+    }
+  });
+  
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+  
