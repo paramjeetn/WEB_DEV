b151c963af5e18148185b8be9375616837273c5b
Paramjeet <paramjeetnpradhan@gmail.com>
2023-10-21 10:59:31 +0530

    made a random name generator using ejs

--- EJS/4.4+Band+Generator+Project/4.4 Band Generator Project/index.js
+++ EJS/4.4+Band+Generator+Project/4.4 Band Generator Project/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 
 //Step 3 - Make the styling show up.
 //Hint 1: CSS files are static files!
@@ -14,11 +15,18 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs");
+  
 });
 
 app.post("/submit", (req, res) => {
+  const randmadj=(Math.floor(Math.random()*adj.length));
+  const randmnoun=(Math.floor(Math.random()*noun.length));
+  
+
+  res.render("index.ejs",{first:adj[randmadj],second:noun[randmnoun]});
   //Step 2 - Make the generate name functionality work
   //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
   //Then:
